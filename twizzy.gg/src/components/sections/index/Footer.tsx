0612a2a11115d95fa2a2f7657b747768eaffb478
bigcheesh <126612289+bigcheesh@users.noreply.github.com>
2026-06-08 17:32:52 -0400

    Update Footer.tsx

--- twizzy.gg/src/components/sections/index/Footer.tsx
+++ twizzy.gg/src/components/sections/index/Footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
             <footer className="max-w-4xl w-full flex flex-col mx-auto pt-4 pb-2">
                 <Divider />
                 <p className="text-center font-semibold text-base pt-2">
-                    Twizzy.gg - Made with NextJS, TailwindCSS, and ❤ by me
+                    Twizzy.gg - Made with NextJS, TailwindCSS, and ❤ by me (lix.bear)
                 </p>
                 <p className="text-center font-medium brightness-75 text-base">
-                    Last updated: May 22nd, 2026
+                    Last updated: June 8th, 2026
                 </p>
             </footer>
         </>
